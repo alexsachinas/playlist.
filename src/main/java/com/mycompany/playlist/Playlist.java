0b3571541606fed8ad15d68c5f61797f5a7664cb
alexs <alexs@alexsachias09>
2024-05-30 20:04:01 -0600

    para el profesor alfonso

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author alexs
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas p1=new Musicas("Mr. Brownstone", "Appetite For Destruction", "1987", 4);
+        p1.ShowPlaylist();
+        }
+}
+
